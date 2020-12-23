e4f914bff6666b2dc11982a4bc53c1babca4b5d7
haeunsong <jamy0225@naver.com>
2020-12-23 15:02:41 +0900

    user정보를 firebase에서 받아올 때까지 기다리기 완료!

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
-import React,{useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from "components/Router";
-import {authService} from "fbase";
+import { authService } from "fbase";
 import { auth } from 'firebaseui';
 
 const App = () => {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  // firebase가 정보를 다 업데이트해서 authService.currentUser가 null값이 아닌 
+  // 사용자 정보가 들어있을 때까지 기다려준다. 
+  useEffect(() => {
+    // 이벤트를 listen 해야함.
+    // onAuthStateChanged (관찰자)
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
 
+  }, [])
   return (
-    <div>
-      <AppRouter isLoggedIn={isLoggedIn} />;
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."};
       <footer>&copy; {new Date().getFullYear()}Nwitter</footer>
-      
-    </div>
+
+    </>
   );
 };
 
